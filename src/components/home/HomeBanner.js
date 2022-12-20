dede5b38e4ce141201969053bd73e2760c5e717c
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2022-12-20 11:45:36 +0600

    Listing

--- src/components/home/HomeBanner.js
+++ src/components/home/HomeBanner.js
@@ -1,33 +1,87 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const HomeBanner = () => {
-  const [color, setColor] = useState("Orange");
+  const [todo, setTodo] = useState([]);
 
-  if (color === "Red") {
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodo(res.data);
+      })
+      .catch(() => {});
+  }, []);
+
+  const deleteEvent = (id) => {
+    let deletId = "Delete ID = " + id;
+    alert(deletId);
+    // API CAll Delete
+  };
+
+  const editEvent = (id) => {
+    let editId = "Edit ID = " + id;
+    alert(editId);
+    // API CAll Edit
+  };
+
+  const detailsEvent = (id) => {
+    let detailsId = "Details ID = " + id;
+    alert(detailsId);
+    // API CAll Details
+  };
+
+  const myList = todo.map((list) => {
     return (
-      <div>
-        <h1 style={{ color: "Red" }}>This is Red Color</h1>
-      </div>
+      <tr>
+        <td>{list.id}</td>
+        <td>{list.title}</td>
+        <td>
+          <button
+            onClick={deleteEvent.bind(this, list.id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </td>
+        <td>
+          <button
+            onClick={editEvent.bind(this, list.id)}
+            className="btn btn-success"
+          >
+            Edit
+          </button>
+        </td>
+        <td>
+          <button
+            onClick={detailsEvent.bind(this, list.id)}
+            className="btn btn-primary"
+          >
+            Details
+          </button>
+        </td>
+      </tr>
     );
-  } else if (color === "Blue") {
-    return (
-      <div>
-        <h1 style={{ color: "Blue" }}>This is Blue Color</h1>
-      </div>
-    );
-  } else if (color === "Green") {
-    return (
-      <div>
-        <h1 style={{ color: "Green" }}>This is Green Color</h1>
-      </div>
-    );
-  } else if (color === "Orange") {
-    return (
-      <div>
-        <h1 style={{ color: "Orange" }}>This is Orange Color</h1>
-      </div>
-    );
-  }
+  });
+
+  return (
+    <div>
+      <table className="table table-striped mx-2">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Title</td>
+            <td>Delete</td>
+            <td>Edit</td>
+            <td>Details</td>
+          </tr>
+        </thead>
+        <tbody>{myList}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default HomeBanner;
