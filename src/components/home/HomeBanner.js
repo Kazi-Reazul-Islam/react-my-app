ef7b406ab1c62fd504d8a41ec68f47244fa9417a
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2022-12-22 15:26:04 +0600

    ComponentDidMount Method Using ClassComponent

--- src/components/home/HomeBanner.js
+++ src/components/home/HomeBanner.js
@@ -1,15 +1,50 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 class HomeBanner extends Component {
   constructor() {
     super();
+    this.state = {
+      toDoList: [],
+    };
   }
   componentDidMount() {
-    alert("Hello");
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/")
+      .then((res) => {
+        this.setState({ toDoList: res.data });
+      })
+      .catch((err) => {
+        alert(err);
+      });
   }
 
   render() {
-    return <div></div>;
+    const myList = this.state.toDoList.map((item) => {
+      return (
+        <tr>
+          <td>{item.id}</td>
+          <td>{item.title}</td>
+        </tr>
+      );
+    });
+    return (
+      <div>
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              <td>
+                <strong>Id</strong>
+              </td>
+              <td>
+                <strong>Title</strong>
+              </td>
+            </tr>
+          </thead>
+          <tbody>{myList}</tbody>
+        </table>
+      </div>
+    );
   }
 }
 
